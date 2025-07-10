e126d163142cf851cc9971fb59ab516dab2addde
minseon9 <minseon9@users.noreply.github.com>
2025-07-10 13:30:31 +0000

    deploy: 3fd3b23990fa43f5ed919d65c098940e76e5d2ab

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -48,13 +48,13 @@ var store = [{
         "teaser": null
       },{
         "title": "Two Sum(Easy)",
-        "excerpt":"Two Sum 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Two Sum 문제는 단순하다. integet 배열(nums)이 주어지고, integer의 목표값(target)이 주어진다. 배열에서 합이 target이 되는 두 element의 index를 반환하면 된다. 가령 Input: nums = [3,2,4], target = 6 Output: [1,2] 문제 풀이 문제는 a+b=x인 a,b를 찾는 것이다. 2가지 관점에서 생각해볼...","categories": ["Leetcode"],
+        "excerpt":"Two Sum 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Reference Two Sum 문제는 단순하다. integet 배열(nums)이 주어지고, integer의 목표값(target)이 주어진다. 배열에서 합이 target이 되는 두 element의 index를 반환하면 된다. 가령 Input: nums = [3,2,4], target = 6 Output: [1,2] 문제 풀이 문제는 a+b=x인 a,b를 찾는 것이다. 2가지 관점에서...","categories": ["Leetcode"],
         "tags": ["Array","HashTable"],
         "url": "/wiki/TwoSum",
         "teaser": null
       },{
         "title": "코드 변환기로 새로운 언어 익히기",
-        "excerpt":"나의 동료 AI Prompt Cursor Setting 실례 그 동안 다루지 않았던 언어를 익히려고 한다. 제품 개발을 하면서, 업무를 하면서 새로운 언어를 익히는 것은 비교적 빠르게 할 수 있다. 잘(?) 짜여진 코드들이 이미 가득한 곳을 헤엄치다 보면 익숙해지기 마련이니까. 나처럼 방구석에 앉아, 딱히 뭘 만들지도 않으면서 익히기란 매우 곤욕이다. 그래서 Leetcode...","categories": ["AI"],
+        "excerpt":"나의 동료 AI Prompt Cursor Setting 실례 Reference 그 동안 다루지 않았던 언어를 익히려고 한다. 제품 개발을 하면서, 업무를 하면서 새로운 언어를 익히는 것은 비교적 빠르게 할 수 있다. 잘(?) 짜여진 코드들이 이미 가득한 곳을 헤엄치다 보면 익숙해지기 마련이니까. 나처럼 방구석에 앉아, 딱히 뭘 만들지도 않으면서 익히기란 매우 곤욕이다. 그래서...","categories": ["AI"],
         "tags": ["Cursor","Prompt"],
         "url": "/wiki/Code%20Translator",
         "teaser": null
@@ -66,55 +66,55 @@ var store = [{
         "teaser": null
       },{
         "title": "Add Two Numbers(Med.)",
-        "excerpt":"Add Two Numbers 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Add Two Numbers 두개의 non-negative interger가 들어있는 Linked Lists가 주어진다. 각 자리수들은 1의 자리부터 각 Linked Lists에 저장되어 있다. 문제는 두 Linked List의 non-negative interger의 합을 동일한 Linked Lists로 반환하는 것이다. 가령 Input: l1 = [2,4,3], l2 =...","categories": ["Leetcode"],
+        "excerpt":"Add Two Numbers 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Reference Add Two Numbers 두개의 non-negative interger가 들어있는 Linked Lists가 주어진다. 각 자리수들은 1의 자리부터 각 Linked Lists에 저장되어 있다. 문제는 두 Linked List의 non-negative interger의 합을 동일한 Linked Lists로 반환하는 것이다. 가령 Input: l1 = [2,4,3], l2...","categories": ["Leetcode"],
         "tags": ["LinkedList","Math"],
         "url": "/wiki/AddTwoNumbers",
         "teaser": null
       },{
         "title": "Longest Substring Without Repeating Characters(Med.)",
-        "excerpt":"Longest Substring Without Repeating Characters Substring &amp; Subsequence 문제 풀이 Sliding Window 상세 Code Kotlin Python Leetcode Solution Link Longest Substring Without Repeating Characters 하나의 문자열이 주어진다. 해당 문자열에서 중복된 문자가 없는 가장 긴 substring의 “길이”를 찾으면 된다. 가령 Input: s = \"abcabcbb\" Output: 3 Substring &amp; Subsequence substring 문제다....","categories": ["Leetcode"],
+        "excerpt":"Longest Substring Without Repeating Characters Substring &amp; Subsequence 문제 풀이 Sliding Window 상세 Code Kotlin Python Leetcode Solution Link Reference Longest Substring Without Repeating Characters 하나의 문자열이 주어진다. 해당 문자열에서 중복된 문자가 없는 가장 긴 substring의 “길이”를 찾으면 된다. 가령 Input: s = \"abcabcbb\" Output: 3 Substring &amp; Subsequence substring...","categories": ["Leetcode"],
         "tags": ["String","HashTable","SlidingWindow"],
         "url": "/wiki/LongestSubstringWithoutRepeatingCharacters",
         "teaser": null
       },{
         "title": "Median of Two Sorted Arrays(Hard)",
-        "excerpt":"Median of Two Sorted Arrays 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Median of Two Sorted Arrays 두개의 정렬이 되어 있는 배열이 주어진다. 두 정렬된 배열의 특정 원소를 찾는 문제로, 이번에는 중앙값(median)을 찾으면 된다. 가령 Input: nums1 = [1,3], nums2 = [2] Output: 2.00000 문제 풀이 쉬운 방법은...","categories": ["Leetcode"],
+        "excerpt":"Median of Two Sorted Arrays 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Reference Median of Two Sorted Arrays 두개의 정렬이 되어 있는 배열이 주어진다. 두 정렬된 배열의 특정 원소를 찾는 문제로, 이번에는 중앙값(median)을 찾으면 된다. 가령 Input: nums1 = [1,3], nums2 = [2] Output: 2.00000 문제 풀이 쉬운...","categories": ["Leetcode"],
         "tags": ["Array","BinarySearch","DivideAndConquer"],
         "url": "/wiki/MedianOfTwoSortedArray",
         "teaser": null
       },{
         "title": "Longest Palindrome Substring(Med.)",
-        "excerpt":"Longest Palindrome Substring 문제 풀이 Manacher’s Algorithm 상세 Trick Code Kotlin Python Leetcode Solution Link Longest Palindrome Substring 주어진 문자열에 대해 가장 긴 Palindrome을 찾는 문제다. 가령 Input: s = \"babad\" Output: \"bab\" Palindrome은 거꾸로 읽어도, 똑바로 읽어도 똑같은 회문을 말한다. 토마토, 기러기, 스위스, 우영우, 역삼역 문제 풀이 이미 탐색한...","categories": ["Leetcode"],
+        "excerpt":"Longest Palindrome Substring 문제 풀이 Manacher’s Algorithm 상세 Trick Code Kotlin Python Leetcode Solution Link Reference Longest Palindrome Substring 주어진 문자열에 대해 가장 긴 Palindrome을 찾는 문제다. 가령 Input: s = \"babad\" Output: \"bab\" Palindrome은 거꾸로 읽어도, 똑바로 읽어도 똑같은 회문을 말한다. 토마토, 기러기, 스위스, 우영우, 역삼역 문제 풀이 이미...","categories": ["Leetcode"],
         "tags": ["Manacher'sAlgorithm","TowPointers","String","DynamicProgramming"],
         "url": "/wiki/LongestPalindromeSubstring",
         "teaser": null
       },{
         "title": "Reversed Integer(Med.)",
-        "excerpt":"Reversed Integer 문제 풀이 상세 Overflow Code Kotlin Python Leetcode Solution Link Reversed Integer signed 32-bit integer가 주어진다. 주어진 정수를 뒤집으면 된다. 가령 Input: x = -123 Output: -321 단, 뒤집었을 때 signed 32-bit 범위를 넘어가면 0을 반환한다. 문제 풀이 10진수 정수를 뒤집는 방법은 매우 유명하다. 10으로 나눈 나머지로 새로운...","categories": ["Leetcode"],
+        "excerpt":"Reversed Integer 문제 풀이 상세 Overflow Code Kotlin Python Leetcode Solution Link Reference Reversed Integer signed 32-bit integer가 주어진다. 주어진 정수를 뒤집으면 된다. 가령 Input: x = -123 Output: -321 단, 뒤집었을 때 signed 32-bit 범위를 넘어가면 0을 반환한다. 문제 풀이 10진수 정수를 뒤집는 방법은 매우 유명하다. 10으로 나눈 나머지로...","categories": ["Leetcode"],
         "tags": ["Math"],
         "url": "/wiki/ReversedInteger",
         "teaser": null
       },{
         "title": "Merge Sorted Array(Easy)",
-        "excerpt":"Merge Sorted Array 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Merge Sorted Array 주어진 두 정수 배열(nums1, nums2)을 정렬된 하나의 배열로 합치는 문제다. 각 배열에 대해 m, n이 주어지는데 이는 정렬해야할 배열의 크기이다. 특이한 점은 새로운 배열로 합지는 것이 아니라 nums1에 합쳐야한다는 것이다. nums1는 m개의 element를 가지고 있고,...","categories": ["Leetcode"],
+        "excerpt":"Merge Sorted Array 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Reference Merge Sorted Array 주어진 두 정수 배열(nums1, nums2)을 정렬된 하나의 배열로 합치는 문제다. 각 배열에 대해 m, n이 주어지는데 이는 정렬해야할 배열의 크기이다. 특이한 점은 새로운 배열로 합지는 것이 아니라 nums1에 합쳐야한다는 것이다. nums1는 m개의 element를 가지고...","categories": ["Leetcode"],
         "tags": ["Array","Sorting","TwoPointers"],
         "url": "/wiki/MergeSortedArray",
         "teaser": null
       },{
         "title": "Remove Duplicates From Sorted Array(Easy)",
-        "excerpt":"Remove Duplicates From Sorted Array 문제 풀이 상세 Two Pointers Code Kotlin Python Leetcode Solution Link Remove Duplicates From Sorted Array 주어진 정수 배열에서 중복된 인자를 제거하는 문제다. 주어진 배열을 수정해야하며, 중복되지 않은 인자의 갯수를 반환하면 된다. 가령 Input: nums = [1,1,2] Output: 2, nums = [1,2,_] 중복되지 않은 n개의...","categories": ["Leetcode"],
+        "excerpt":"Remove Duplicates From Sorted Array 문제 풀이 상세 Two Pointers Code Kotlin Python Leetcode Solution Link Reference Remove Duplicates From Sorted Array 주어진 정수 배열에서 중복된 인자를 제거하는 문제다. 주어진 배열을 수정해야하며, 중복되지 않은 인자의 갯수를 반환하면 된다. 가령 Input: nums = [1,1,2] Output: 2, nums = [1,2,_] 중복되지 않은...","categories": ["Leetcode"],
         "tags": ["Array","TwoPointers"],
         "url": "/wiki/RemoveDuplicatesFromSortedArray",
         "teaser": null
       },{
         "title": "Binary Tree Inorder Traversal(Med.)",
-        "excerpt":"Binary Tree Inorder Traversal 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Binary Tree Inorder Traversal 주어진 이진 트리를 inorder로 탐색하는 문제다. 가령 Input: root = [1,2,3,4,5,null,8,null,null,6,7,9] Tree: 1 / \\ 2 3 / \\ \\ 4 5 8 / \\ \\ 6 7 9 Output: [4,2,6,5,7,1,3,9,8] 재귀 함수를...","categories": ["Leetcode"],
+        "excerpt":"Binary Tree Inorder Traversal 문제 풀이 상세 Code Kotlin Python Leetcode Solution Link Reference Binary Tree Inorder Traversal 주어진 이진 트리를 inorder로 탐색하는 문제다. 가령 Input: root = [1,2,3,4,5,null,8,null,null,6,7,9] Tree: 1 / \\ 2 3 / \\ \\ 4 5 8 / \\ \\ 6 7 9 Output: [4,2,6,5,7,1,3,9,8] 재귀...","categories": ["Leetcode"],
         "tags": ["Stack","DFS","BinaryTree","Tree"],
         "url": "/wiki/BinaryTreeInorderTraversal",
         "teaser": null
       },{
         "title": "Shuffle an Array",
-        "excerpt":"Shuffle an Array 문제 풀이 Fisher-Yates Shuffle Deep copy Code Kotlin Python Leetcode Solution Link Shuffle an Array 주어진 정수 배열을 무작위로 섞는 알고리즘을 디자인하면 되는 문제다. 모든 순열이 공평하게 생성되어야 한다. 아래의 함수들을 구현하면 된다. int[] reset() 처음 주어진 정수 배열을 반환한다. int[] shuffle() 정수 배열을 무작위로 섞어 반환한다....","categories": ["Leetcode"],
+        "excerpt":"Shuffle an Array 문제 풀이 Fisher-Yates Shuffle Deep copy Code Kotlin Python Leetcode Solution Link Reference Shuffle an Array 주어진 정수 배열을 무작위로 섞는 알고리즘을 디자인하면 되는 문제다. 모든 순열이 공평하게 생성되어야 한다. 아래의 함수들을 구현하면 된다. int[] reset() 처음 주어진 정수 배열을 반환한다. int[] shuffle() 정수 배열을 무작위로 섞어...","categories": ["Leetcode"],
         "tags": ["Array","Math","Design","Randomized"],
         "url": "/wiki/Shuffle%20an%20Array",
         "teaser": null
